13bbf30243659d6e88bca4ed37fd92923a611e82
Prashil1999 <prashilgupta99@gmail.com>
2018-09-21 14:57:49 +0530

    prashil commit till workactivity

--- app/src/main/java/com/eyelens/WorkActivity.java
+++ app/src/main/java/com/eyelens/WorkActivity.java
@@ -1,18 +1,23 @@
 package com.eyelens;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class WorkActivity extends AppCompatActivity {
-
+    Bitmap photo;
+    ImageView imgFinal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work);
 
+        imgFinal=findViewById(R.id.img_final);
 
         Intent i=getIntent();
-        i.getB
+        photo=(Bitmap) i.getExtras().get("PHOTO");
+        imgFinal.setImageBitmap(photo);
     }
 }
